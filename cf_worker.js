b786045c32e5ec5be08972dc5e3ea06993607b6a
Jing Zhe <jzshow@gmail.com>
2023-05-20 11:41:21 +0800

    Update cf_worker.js
    添加IP限制

--- cf_worker.js
+++ cf_worker.js
@@ -1,10 +1,24 @@
 const TELEGRAPH_URL = 'https://api.openai.com';
 
+const allowed_ip_address = ['0.0.0.0', '127.0.0.1']
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
+  
+  // 判断IP是否允许访问此接口
+  /*
+  const ip_address = request.headers.get('cf-connecting-ip');
+  if (blocked_ip_address.includes(ip_address)) {
+      let response = new Response('Access denied: Your IP address is blocked by Server.', {
+          status: 403
+      });
+      return response
+  }
+  */
+  
   const url = new URL(request.url);
   const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
   url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
